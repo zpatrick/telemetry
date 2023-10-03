f32f70fcabd60f02b062177718fec474160bc0db
Zack Patrick <zack.patrick@outreach.io>
2023-10-03 14:34:57 -0700

    update trace

--- trace/trace.go
+++ trace/trace.go
@@ -2,17 +2,26 @@ package trace
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/zpatrick/telemetry/tag"
 )
 
 type Tracer interface {
 	Start(ctx context.Context, name string, tags ...tag.Tag) context.Context
+	End(ctx context.Context)
+
 	AddTags(ctx context.Context, tags ...tag.Tag) // context.Context?
-	Finish(ctx context.Context)
+	Err(ctx context.Context, err error)
+
+	TraceHandler(h http.Handler) http.Handler
+	RoundTripper(r http.RoundTripper) http.RoundTripper
 }
 
 func Error(ctx context.Context, t Tracer, err error) error {
 	// Adds error metadata to existing trace.
 	return err
 }
+
+// TODO: add a middleware that adds trace metadata to the context.
+// TODO: add a http client round tripper which adds middleware.
